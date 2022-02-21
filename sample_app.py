56a3b4562976915e7d6019b4fe91b06393211429
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2022-02-20 21:38:54 -0800

    Recursive scroll scraper

---
+++ sample_app.py
@@ -0,0 +1,42 @@
+"""
+A sample application demonstrating RecursiveScrollScrape usage.
+"""
+from typing import Optional
+import sys
+from bs4 import BeautifulSoup
+from tq_recursive_scroll_scrape.recursive_scroll_and_scrape import RecursiveScrollScrape
+
+
+def main() -> int:
+    """
+    The sample application entry point.
+    :return: The operating system exit code.
+    """
+    root_url = "https://www.trulia.com"
+    first_url = f"{root_url}/WA/Renton"
+
+    scroll_scraper = RecursiveScrollScrape()
+
+    def _on_after_download(content: str):
+        print(f"on_after_download ==> content length ==> {len(content)}")
+
+    def _get_next_url(content: str) -> Optional[str]:
+        soup = BeautifulSoup(content, "html.parser")
+
+        links = [a for a in soup.find_all("a")
+                 if a.get("aria-label")
+                 and "Next" in a.get("aria-label")]
+
+        if len(links) == 0:
+            return None
+
+        next_url = f"{root_url}{links[0].get('href')}"
+        print(f"_get_next_url ==> {next_url}")
+        return next_url
+
+    scroll_scraper.download(first_url, _on_after_download, _get_next_url)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ test/test_setup.py
@@ -0,0 +1,9 @@
+"""
+This file only tests the import statements work.
+"""
+
+from tq_recursive_scroll_scrape.recursive_scroll_and_scrape import RecursiveScrollScrape
+
+
+def test_foo():
+    assert 1 == 1
